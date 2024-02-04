071a2fbeec691a93b1eb76603cf988826320b638
amirHossein5 <amir5665@yahoo.com>
2024-02-04 22:25:05 +0330

    Refactor index page by adding layout

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -1,13 +1,6 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>ticketstore</title>
+<x-app-layout>
+    <x-slot:title>ticketstore</x-slot>
 
-    @vite(['resources/css/app.css'])
-</head>
-<body class='bg-[#26262D]'>
     <section class="flex items-center flex-col gap-5 mt-20">
         @foreach(range(1,4) as $i)
             <div class="flex flex-row w-[800px]">
@@ -21,7 +14,7 @@
                     </div>
 
                     <form class="flex flex-row gap-3 items-center" action="{{ route('purchase', $i)}}">
-                        <input type="text" default="1" name="quantity" class="bg-[#43434E] rounded w-14 h-8 border-none text-gray-200 placeholder:text-center" placeholder="1">
+                        <x-text-input value="1" name="quantity" class="w-14 h-8 text-center"/>
                         <div class="text-gray-200">X</div>
                         <div class="text-gray-200">$25.00</div>
                         <button class="bg-[#076074] rounded w-14 h-8 flex justify-center items-center text-gray-200">Buy</button>
@@ -30,5 +23,4 @@
             </div>
         @endforeach
     </section>
-</body>
-</html>
+</x-app-layout>
